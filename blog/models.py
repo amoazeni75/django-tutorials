ade3593ea43bf03b015612a81f4b4d1dfceaf911
amoazeni75 <amoazeni75@gmail.com>
2020-04-11 22:37:49 +0430

    -creating post and comment models

--- blog/models.py
+++ blog/models.py
@@ -1,1 +1,48 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+
+
+# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approve_comment(self):
+        return self.comments.filter(approved_comment=True)  # comments come from the *1
+
+    def __str__(self):
+        return self.title
+
+    """
+        This is for the cases that after creating a post, where should take the user
+    """
+
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'pk': self.pk})
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')  # *1: related_name: when in
+    # the post class call comments, return all comments which their foreign key belongs to the post
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse('post_list')
+
+    def __str__(self):
+        return self.text
