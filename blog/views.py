ad1c932aea1c099e0d2f3d9c423e10c275f73a62
amoazeni75 <amoazeni75@gmail.com>
2020-04-12 09:48:02 +0430

    -adding AboutView and PostListView -setting up the urls for the two new view class

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
-from django.views.generic import (TemplateView)
+from django.views.generic import (TemplateView, ListView)
+from blog.models import Post, Comment
+from django.utils import timezone
+
+
 # Create your views here.
 
 class AboutView(TemplateView):
     template_name = 'about.html'
+
+
+class PostListView(ListView):
+    model = Post
+
+    """
+    This helps to use Django's ORM when dealing with such generic view, it is a version of
+    writing SQL query in python
+    """
+
+    def get_queryset(self):
+        """:param
+        actually we get all the posts which their published date is less or equal to the now
+        and sort them by the time near to now(the most recent post comes up front or at the top of the list)
+        (using mines behind the field).
+        when define the following function, instead of retuning whole objects, it will run your sql
+        query to get the objects. for more information check out the db->query in the
+        django documentation page
+        """
+        return Post.objects.filter(published_date__lte=timezone.now().order_by('-published_date'))
