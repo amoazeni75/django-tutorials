e39670d02b40f2c48c1c55059949fa81627d99d7
amoazeni75 <amoazeni75@gmail.com>
2020-04-17 20:58:58 +0430

    -adding list display field to the admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -8,6 +8,7 @@ class CommentAdmin(admin.ModelAdmin):
     fields = ['author', 'created_date', 'approved_comment', 'text', 'post']
     search_fields = ['author', 'text', 'post__title']  # add searching by tittle in the admin panel
     list_filter = ['post']
+    list_display = ['author', 'created_date', 'post']
 
 
 admin.site.register(Post)
