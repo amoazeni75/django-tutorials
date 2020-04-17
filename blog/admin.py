99b55c6faf7a7b258002fe7d21dd85026f2da9a6
amoazeni75 <amoazeni75@gmail.com>
2020-04-17 19:35:23 +0430

    -change the showing order of comment's fields in the admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from blog.models import Post, Comment
 
+
 # Register your models here.
+
+class CommentAdmin(admin.ModelAdmin):
+    fields = ['author', 'created_date', 'approved_comment', 'text', 'post']
 
 
 admin.site.register(Post)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
