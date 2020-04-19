b133dd9dd46fa113c32e88bd05f5cf6d0ad63562
amoazeni75 <amoazeni75@gmail.com>
2020-04-19 16:46:10 +0430

    -creating article model

---
+++ newsapi/news/api/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+
+class ArticleSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    author = serializers.CharField()
+    title = serializers.CharField()
+    description = serializers.CharField()
+    body = serializers.CharField()
+    location = serializers.CharField()
+    pub
