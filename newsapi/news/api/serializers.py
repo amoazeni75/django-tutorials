0e52fa1b2f30c337a6c94acd9f7a6214ab757b4a
amoazeni75 <amoazeni75@gmail.com>
2020-04-21 13:55:44 +0430

    - defining the SerializerModel for Article - specifying the field wants to show - adding serializer field

--- newsapi/news/api/serializers.py
+++ newsapi/news/api/serializers.py
@@ -1,41 +1,79 @@
 from rest_framework import serializers
 from news.models import Article
 
+from django.utils.timesince import timesince
+from datetime import datetime
 
-class ArticleSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    author = serializers.CharField()
-    title = serializers.CharField()
-    description = serializers.CharField()
-    body = serializers.CharField()
-    location = serializers.CharField()
-    publication_date = serializers.DateField()
-    active = serializers.BooleanField()
-    created_at = serializers.DateTimeField(read_only=True)
-    updated_at = serializers.DateTimeField(read_only=True)
+"""
+The following class represent creating a serializer class for a specific model class,
+for example we write ArticleSerializerClass to serialize the Article Model which we implemented it.
+Most of the functions and fields should be writen by us; actually, this is the simplest and basic way
+"""
 
-    def create(self, validated_data):
-        return Article.objects.create(**validated_data)
+# class ArticleSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     author = serializers.CharField()
+#     title = serializers.CharField()
+#     description = serializers.CharField()
+#     body = serializers.CharField()
+#     location = serializers.CharField()
+#     publication_date = serializers.DateField()
+#     active = serializers.BooleanField()
+#     created_at = serializers.DateTimeField(read_only=True)
+#     updated_at = serializers.DateTimeField(read_only=True)
+#
+#     def create(self, validated_data):
+#         return Article.objects.create(**validated_data)
+#
+#     def update(self, instance, validated_data):
+#         instance.author = validated_data.get('author', instance.author)
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.body = validated_data.get('body', instance.body)
+#         instance.location = validated_data.get('location', instance.location)
+#         instance.publication_date = validated_data.get('publication_date', instance.publication_date)
+#         instance.active = validated_data.get('active', instance.active)
+#         instance.save()
+#         return instance
+#
+#     def validate(self, data):
+#         """This function is a global validator for all of the parameters"""
+#         if data["title"] == data["description"]:
+#             raise serializers.ValidationError("Title and Description must be different from each other!")
+#         return data
+#
+#     def validate_title(self, value):
+#         """This function is a validator just for title field"""
+#         if len(value) < 60:
+#             raise serializers.ValidationError("The title must include at least 60 characters")
+#         return value
 
-    def update(self, instance, validated_data):
-        instance.author = validated_data.get('author', instance.author)
-        instance.title = validated_data.get('title', instance.title)
-        instance.description = validated_data.get('description', instance.description)
-        instance.body = validated_data.get('body', instance.body)
-        instance.location = validated_data.get('location', instance.location)
-        instance.publication_date = validated_data.get('publication_date', instance.publication_date)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
 
-    def validate(self, data):
-        """This function is a global validator for all of the parameters"""
-        if data["title"] == data["description"]:
-            raise serializers.ValidationError("Title and Description must be different from each other!")
-        return data
+"""
+The ModelSerializerClass is the other way of serializing a model. This way includes less code
+and some functions like create, update, etc. is ready to use. This way also provides automatic definition
+of the fields
 
-    def validate_title(self, value):
-        """This function is a validator just for title field"""
-        if len(value) < 60:
-            raise serializers.ValidationError("The title must include at least 60 characters")
-        return value
+"""
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    # you can also define some fields added to the model's fields
+    # 1: define the field you want to add to the fields
+    # 2: define the get_name_of_ne_field() method
+    time_since_publication = serializers.SerializerMethodField()
+
+    class Meta:
+        # 1: specify the model which wants to serialize
+        model = Article
+
+        # 2: specify the fields which wants to serialize
+        # fields = "__all__"  # we want all the fields of our model
+        # fields = ("title", "body", "description")  # we want to serialize just these fields
+        exclude = ('id',)  # we want all the fields except these fields
+
+    def get_time_since_publication(self, object):
+        p_date = object.publication_date
+        now = datetime.now()
+        t_delta = timesince(p_date, now)
+        return t_delta
--- newsapi/news/api/urls.py
+++ newsapi/news/api/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from news.api.views import article_list_create_api_view, article_detail_api_view, ArticleDetailAPIView, \
-    ArticleListCreateAPIView
+from news.api.views import (article_list_create_api_view, article_detail_api_view,
+                            ArticleDetailAPIView, ArticleListCreateAPIView,
+                            Article)
 
 urlpatterns = [
+    # 1: function based API
     # path("articles/", article_list_create_api_view, name='article-list'),
     # path("articles/<int:pk>/", article_detail_api_view, name='article-detail'),
+
+    # 2: class based API, it self has two model for its serializer
+    # 2-1: defining a class which inherit serializer class and implementing all things by ourselves
+    # 2-2: using ModelsSerializer which is very simple and less code needed
     path("articles/", ArticleListCreateAPIView.as_view(), name='article-list'),
     path("articles/<int:pk>/", ArticleDetailAPIView.as_view(), name='article-detail'),
 ]
