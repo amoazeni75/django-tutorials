aaddf54e87a14b1d4a1b3e6eb5e272e2c561cf40
amoazeni75 <amoazeni75@gmail.com>
2020-04-22 16:43:38 +0430

    -Review create, list, update, delete views are ready

--- ebooksapi/ebooks/api/urls.py
+++ ebooksapi/ebooks/api/urls.py
@@ -1,7 +1,22 @@
 from django.urls import path
-from ebooks.api.views import EookListCreateAPIViewAdvanced
+from ebooks.api.views import (EookListCreateAPIViewAdvanced, EbookDetailAPIView,
+                              ReviewCreateAPIView, ReviewDetailAPIView)
 
 urlpatterns = [
     # path('ebooks/', EbookListCreateAPIView.as_view(), name='ebooks-list')
-    path('ebooks/', EookListCreateAPIViewAdvanced.as_view(), name='ebooks-list')
+    path('ebooks/',
+         EookListCreateAPIViewAdvanced.as_view(),
+         name='ebooks-list'),
+
+    path('ebooks/<int:pk>',
+         EbookDetailAPIView.as_view(),
+         name='ebooks-detail'),
+
+    path('ebooks/<int:ebook_pk>/review',
+         ReviewCreateAPIView.as_view(),
+         name='ebooks-review'),
+
+    path('review/<int:pk>',
+         ReviewDetailAPIView.as_view(),
+         name='review-detail'),
 ]
--- ebooksapi/ebooks/api/views.py
+++ ebooksapi/ebooks/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework import mixins
+from rest_framework.generics import get_object_or_404
 
 from ebooks.models import Ebook, Review
 from ebooks.api.serializers import EbookSerializer, ReviewSerializer
@@ -8,26 +8,50 @@ from ebooks.api.serializers import EbookSerializer, ReviewSerializer
 In the following class we need to implement get, post method 
 """
 
-
-class EbookListCreateAPIView(mixins.ListModelMixin,
-                             mixins.CreateModelMixin,
-                             generics.GenericAPIView):
-    # 1: we should define two bellow things
-    queryset = Ebook.objects.all()
-    serializer_class = EbookSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+# class EbookListCreateAPIView(mixins.ListModelMixin,
+#                              mixins.CreateModelMixin,
+#                              generics.GenericAPIView):
+#     # 1: we should define two bellow things
+#     queryset = Ebook.objects.all()
+#     serializer_class = EbookSerializer
+#
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+#
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
 
 
 """ 
-The following class is same as the above class, but it has more ready-to-use codes
+The following classes is same as the above class, but it has more ready-to-use codes
 """
 
 
 class EookListCreateAPIViewAdvanced(generics.ListCreateAPIView):
     queryset = Ebook.objects.all()
     serializer_class = EbookSerializer
+
+
+class EbookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Ebook.objects.all()
+    serializer_class = EbookSerializer
+
+
+class ReviewCreateAPIView(generics.CreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    """
+    we must override the following method, because it is for a general usage
+    and we need to specify the ebook foreign key before serializer.save()
+    """
+
+    def perform_create(self, serializer):
+        ebook_pk = self.kwargs["ebook_pk"]
+        ebook = get_object_or_404(Ebook, pk=ebook_pk)
+        serializer.save(ebook=ebook)
+
+
+class ReviewDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
