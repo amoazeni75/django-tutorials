19b6afd9140ee3c96eaae095eba6663936401c3a
amoazeni75 <amoazeni75@gmail.com>
2020-04-23 00:38:53 +0430

    -create signal and sender + register in app config

---
+++ profilesapi/profiles/signals.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from profiles.models import Profile
+
+
+# every time an user created, the following function would be called automatically
+# and create an user_profile instance
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    print("Created: ", created)
+    if created:
+        Profile.objects.create(user=instance)
