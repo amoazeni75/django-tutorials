25d0a44b2e2d279dfb8ab2fcde2fc500cd8fc273
amoazeni75 <amoazeni75@gmail.com>
2020-04-24 15:52:07 +0430

    -permissions file is ready -define a view based on mixins for profile -define a view for updating the avatar profile picture

--- profilesapi/profiles/api/urls.py
+++ profilesapi/profiles/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import include, path
-from profiles.api.views import ProfileViewSet
+from profiles.api.views import ProfileViewSet, ProfileStatusViewSet, AvatarUpdateView
 from rest_framework.routers import DefaultRouter
 
 profile_list = ProfileViewSet.as_view({"get": "list"})
@@ -7,6 +7,7 @@ profile_detail = ProfileViewSet.as_view({"get": "retrieve"})
 
 router = DefaultRouter()
 router.register(r"profiles", ProfileViewSet)
+router.register(r"status", ProfileStatusViewSet)
 
 urlpatterns = [
     # path('profiles/', ProfileList.as_view(), name='profiles-list'),
@@ -15,4 +16,5 @@ urlpatterns = [
     # path('profile/<int:pk>/', profile_detail, name='profile-detail'),
 
     path("", include(router.urls)),
+    path("avatar/", AvatarUpdateView.as_view(), name="avatar-update"),
 ]
--- profilesapi/profiles/api/views.py
+++ profilesapi/profiles/api/views.py
@@ -1,20 +1,60 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from profiles.models import Profile
-from profiles.api.serializers import ProfileSerializer
+from profiles.models import Profile, ProfileStatus
+from profiles.api.serializers import ProfileSerializer, ProfileStatusSerializer, ProfileAvatarSerializer
 
-from rest_framework.viewsets import ReadOnlyModelViewSet
+# from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
+from rest_framework import mixins
+
+from profiles.api.permissions import IsOwnProfileOrReadOnly, IsOwnerOrReadOnly
 
 
 # viewsets class combine both ListView class and DetailView class in one class
 
-class ProfileList(generics.ListAPIView):
-    queryset = Profile.objects.all()
-    serializer_class = ProfileSerializer
-    permission_classes = [IsAuthenticated]
+# level 1
+# class ProfileList(generics.ListAPIView):
+#     queryset = Profile.objects.all()
+#     serializer_class = ProfileSerializer
+#     permission_classes = [IsAuthenticated]
 
 
-class ProfileViewSet(ReadOnlyModelViewSet):
+# level 2
+# class ProfileViewSet(ReadOnlyModelViewSet):
+#     queryset = Profile.objects.all()
+#     serializer_class = ProfileSerializer
+#     permission_classes = [IsAuthenticated]
+
+
+# level 3
+# can see the list of objects and update each of them also see a specific object detail
+class ProfileViewSet(mixins.UpdateModelMixin,
+                     mixins.ListModelMixin,
+                     mixins.RetrieveModelMixin,
+                     viewsets.GenericViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated, IsOwnProfileOrReadOnly]
+
+
+# with ModelViewSet can create, read, update, and delete (CRUD) an instance of a model
+class ProfileStatusViewSet(ModelViewSet):
+    queryset = ProfileStatus.objects.all()
+    serializer_class = ProfileStatusSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    # to connect automatically the new profile status to the profile of the user making request
+    # we have to override the perform_create method
+    def perform_create(self, serializer):
+        user_profile = self.request.user.profile
+        serializer.save(user_profile=user_profile)
+
+
+class AvatarUpdateView(generics.UpdateAPIView):
+    serializer_class = ProfileAvatarSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        profile_object = self.request.user.profile
+        return profile_object
