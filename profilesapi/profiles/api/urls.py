b813be4bc5afabdc7ae9d317ad9bd17da6aa4af9
amoazeni75 <amoazeni75@gmail.com>
2020-04-24 15:15:04 +0430

    -adding router which automatically generates all the related urls for list, detail ...

--- profilesapi/profiles/api/urls.py
+++ profilesapi/profiles/api/urls.py
@@ -1,11 +1,18 @@
-from django.urls import path
+from django.urls import include, path
 from profiles.api.views import ProfileViewSet
+from rest_framework.routers import DefaultRouter
 
 profile_list = ProfileViewSet.as_view({"get": "list"})
 profile_detail = ProfileViewSet.as_view({"get": "retrieve"})
 
+router = DefaultRouter()
+router.register(r"profiles", ProfileViewSet)
+
 urlpatterns = [
     # path('profiles/', ProfileList.as_view(), name='profiles-list'),
-    path('profiles/', profile_list, name='profiles-list'),
-    path('profile/<int:pk>/', profile_detail, name='profile-detail'),
+
+    # path('profiles/', profile_list, name='profiles-list'),
+    # path('profile/<int:pk>/', profile_detail, name='profile-detail'),
+
+    path("", include(router.urls)),
 ]
