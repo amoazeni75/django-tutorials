d61e1a8b61400bc974a1f0d3ba36831d2092a2fb
amoazeni75 <amoazeni75@gmail.com>
2020-04-24 11:54:52 +0430

    -now users can login via api -only the authenticated users can get the list of profiles -ProfileList view implemented -adding profiles' urls to the project's url

--- profilesapi/profilesapi/urls.py
+++ profilesapi/profilesapi/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
+                  path("api/", include("profiles.api.urls")),
                   path("api-auth/", include("rest_framework.urls")),  # for browsable  api
                   path("api/rest-auth/", include("rest_auth.urls")),  # for ajax client
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- profilesapi/token-auth-test1.py
+++ profilesapi/token-auth-test1.py
@@ -3,7 +3,10 @@ import requests
 
 def client():
     credentials = {"username": "admin", "password": "alireza123"}
-    response = requests.post("http://127.0.0.1:8000/api/rest-auth/login", data=credentials)
+    response = requests.post("http://127.0.0.1:8000/api/rest-auth/login/", data=credentials)
     print("Status code: ", response.status_code)
     response_data = response.json()
     print(response_data)
+
+
+client()
