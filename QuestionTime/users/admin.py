ca96c24833d648daa23d3729a9d4e5a2979368b4
amoazeni75 <amoazeni75@gmail.com>
2020-05-03 14:57:04 +0430

    -CustomUser model created

--- QuestionTime/users/admin.py
+++ QuestionTime/users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from users.models import CustomUser
+
 
 # Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    # when we added a new field in the CustomUser, we should update it here for the form
+    # add_form =
+    # form =
+    model = CustomUser
+    list_display = ["username", "email", "is_staff"]
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- QuestionTime/users/models.py
+++ QuestionTime/users/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+
+class CustomUser(AbstractUser):
+    # here you can add any custom fields to the user model
+    pass
