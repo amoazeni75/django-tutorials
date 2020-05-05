50d231b5eb526d6a0f7c69b3201abf53b984b759
amoazeni75 <amoazeni75@gmail.com>
2020-05-05 12:47:49 +0430

    -Auto add slug signals and receiver completed

--- QuestionTime/QuestionTime/settings.py
+++ QuestionTime/QuestionTime/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
+    'questions.apps.QuestionsConfig',
 
     'django.contrib.sites',
 
--- QuestionTime/questions/admin.py
+++ QuestionTime/questions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from questions.models import Question, Answers
 
 # Register your models here.
+
+
+admin.site.register(Question)
+admin.site.register(Answers)
