35a9dd5434ddc200729fd32ddbfa0058e98d566c
amoazeni75 <amoazeni75@gmail.com>
2020-05-12 23:54:20 +0430

    -Navbar component done -base.html created for rapid sharing

--- QuestionTime/frontend/src/App.vue
+++ QuestionTime/frontend/src/App.vue
@@ -1,32 +1,30 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <NavbarComponent/>
+        <router-view/>
     </div>
-    <router-view />
-  </div>
 </template>
 
+<script>
+    import NavbarComponent from "./components/Navbar"
+
+    export default {
+        name: "App",
+        components: {
+            NavbarComponent
+        }
+    }
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+    html, body {
+        height: 100%;
+        font-family: "Playfair Display", serif;
+    }
 
-#nav {
-  padding: 30px;
-}
+    .btn:focus {
+        box-shadow: none !important;
+        /*this is because we do not have refresh in vue, so we need to disable focus*/
+    }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
