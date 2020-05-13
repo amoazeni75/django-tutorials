d08f64700cafb98f4d1d2ca0746474e17f463205
amoazeni75 <amoazeni75@gmail.com>
2020-05-13 22:06:20 +0430

    -Home page completed, getting all questions when Home component created

--- QuestionTime/frontend/src/router/index.js
+++ QuestionTime/frontend/src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-import About from "../views/About.vue";
-
 
 Vue.use(VueRouter);
 
@@ -11,11 +9,6 @@ const routes = [
         path: "/",
         name: "Home",
         component: Home
-    },
-    {
-        path: "/about",
-        name: "About",
-        component: About
     }
 ];
 
