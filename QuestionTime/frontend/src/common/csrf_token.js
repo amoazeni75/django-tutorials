f02a77751f96f929681ed01e41b1202bdc5f3dc7
amoazeni75 <amoazeni75@gmail.com>
2020-05-13 21:40:52 +0430

    -api request via ajax done -csrf_token extraction done

---
+++ QuestionTime/frontend/src/common/csrf_token.js
@@ -0,0 +1,25 @@
+// when making unsafe http request or ajax request, we must include csrf_token
+
+//javascript function to extract csrf_token form cookies, when the user login this field will set
+
+
+// https://docs.djangoproject.com/en/3.0/ref/csrf/
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+var CSRF_TOKEN = getCookie('csrftoken');
+
+export {CSRF_TOKEN};
