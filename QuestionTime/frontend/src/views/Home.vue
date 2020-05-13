6352698e607577a45be62e1d3b909a58bea390ac
amoazeni75 <amoazeni75@gmail.com>
2020-05-13 23:00:20 +0430

    -Single Question component created

--- QuestionTime/frontend/src/views/Home.vue
+++ QuestionTime/frontend/src/views/Home.vue
@@ -4,9 +4,16 @@
             <div v-for="question in questions"
                  :key="question.pk">
                 <p class="mb-0">Posted by:
-                    <span class="author-name">{{question.author}} </span>
+                    <span class="question-author">{{question.author}} </span>
                 </p>
-                <h2>{{question.content}}</h2>
+                <h2>
+                    <router-link
+                            :to="{name :'Question', params : {slug:question.slug}}"
+                            class="question-link"
+                    >
+                        {{question.content}}
+                    </router-link>
+                </h2>
                 <p>Answers: {{question.answers_count}}</p>
                 <hr>
             </div>
@@ -27,7 +34,7 @@
         },
         methods: {
             getQuestions() {
-                let endpoint = "api/questions/"
+                let endpoint = "/api/questions/"
                 apiService(endpoint)
                     .then(data => {
                         this.questions.push(...data.results);
@@ -36,12 +43,23 @@
         },
         created() {
             this.getQuestions();
+            document.title = "QuestionTime"
         }
     };
 </script>
-<style>
-    .author-name {
+<style scoped>
+    .question-author {
         font-weight: bold;
         color: #DC3545;
+    }
+
+    .question-link {
+        font-weight: bold;
+        color: black;
+    }
+
+    .question-link:hover {
+        text-decoration: none;
+        color: #343A40;
     }
 </style>
